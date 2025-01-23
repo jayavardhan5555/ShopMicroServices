b295f202edc410633cf7b77a442a2a2ac519f1a5
jayavardhan5555 <89379481+jayavardhan5555@users.noreply.github.com>
2025-01-23 11:41:48 +0530

    Catalog

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,51 @@
+
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Services to the container
+
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.Run();
